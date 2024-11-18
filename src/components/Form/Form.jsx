ba60317baaf60e8e5c5c3e3bfd0b58cb6e145c7d
Scrappyz <ghostmic3000@gmail.com>
2024-11-18 18:42:55 +0800

    Added show password

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,76 +1,122 @@
-import React from "react";
+import React, { useState, memo } from "react";
 import PropTypes from "prop-types";
 
-function Form(props) {
+// Password Field Component
+const PasswordField = memo(({ field, inputStyle, inputTextStyle, inputBoxStyle }) => {
+    const [showPassword, setShowPassword] = useState(false);
 
+    const togglePasswordVisibility = () => {
+        setShowPassword((prev) => !prev);
+    };
+
+    return (
+        <div style={inputStyle}>
+            {field.label !== null && (
+                <label htmlFor={field.id} style={inputTextStyle}>
+                    {field.label}
+                </label>
+            )}
+            <input
+                id={field.id}
+                type={showPassword ? "text" : "password"}
+                placeholder={field.placeholder}
+                style={inputBoxStyle}
+            />
+            <div>
+                <input type="checkbox" onClick={togglePasswordVisibility} />
+                <label>{field.showPasswordText || "Show Password"}</label>
+            </div>
+        </div>
+    );
+});
+
+// Main Form Component
+function Form(props) {
     const formStyle = {
         ...props.styles.form,
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        height: "auto"
-    }
+        height: "auto",
+    };
 
     const inputContainerStyle = {
         ...props.styles.inputContainer,
         display: "flex",
-        flexDirection: "column"
-    }
+        flexDirection: "column",
+    };
 
     const inputStyle = {
         display: "flex",
         flexDirection: "column",
         margin: "0px",
-        padding: "0px"
-    }
+        padding: "0px",
+    };
 
     const inputTextStyle = {
         ...props.styles.inputText,
         margin: "0",
-        marginBottom: "3px"
-    }
+        marginBottom: "3px",
+    };
 
     return (
         <div style={formStyle}>
-            {props.title === null ? null : <p>{props.title}</p>}
+            {props.title !== null && <p>{props.title}</p>}
             <div style={inputContainerStyle}>
-                {props.fields.map((field) => {
-                    return (
-                        <div style={inputStyle}>
-                            {field.label !== null && <p style={inputTextStyle}>{field.label}</p>}
-                            <input id={field.id} type={field.type} placeholder={field.placeholder} style={props.styles.inputBox}></input>
+                {props.fields.map((field) =>
+                    field.type === "password" ? (
+                        <PasswordField
+                            key={field.id}
+                            field={field}
+                            inputStyle={inputStyle}
+                            inputTextStyle={inputTextStyle}
+                            inputBoxStyle={props.styles.inputBox}
+                        />
+                    ) : (
+                        <div style={inputStyle} key={field.id}>
+                            {field.label !== null && (
+                                <label htmlFor={field.id} style={inputTextStyle}>
+                                    {field.label}
+                                </label>
+                            )}
+                            <input
+                                id={field.id}
+                                type={field.type}
+                                placeholder={field.placeholder}
+                                style={props.styles.inputBox}
+                            />
                         </div>
-                    );
-                })}
+                    )
+                )}
             </div>
         </div>
     );
-    
 }
 
 Form.propTypes = {
     title: PropTypes.string,
     fields: PropTypes.array,
     onSubmit: PropTypes.func,
-    styles: PropTypes.object
-}
+    styles: PropTypes.object,
+};
 
 Form.defaultProps = {
     title: null,
     fields: [
         {
-            id: "usr",
-            label: "wasup",
+            id: "user",
+            label: "Username",
             placeholder: "Username",
-            type: "text"
+            type: "text",
         },
         {
-            id: "usr",
-            label: "pagong",
+            id: "pw",
+            label: "Password",
             placeholder: "Password",
-            type: "text"
-        }
+            type: "password",
+            showPasswordText: "Show Password",
+        },
     ],
     onSubmit: null,
     styles: {
@@ -85,7 +131,7 @@ Form.defaultProps = {
         },
         inputContainer: {
             width: "80%",
-            gap: "10px"
+            gap: "10px",
         },
         inputBox: {
             height: "20px",
@@ -93,12 +139,12 @@ Form.defaultProps = {
             borderColor: "black",
             borderWidth: "1px",
             borderRadius: "5px",
-            width: "97%"
+            width: "97%",
         },
         inputText: {
-            textAlign: "left"
-        }
-    }
-}
+            textAlign: "left",
+        },
+    },
+};
 
 export default Form;
